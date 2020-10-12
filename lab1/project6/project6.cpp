7a86e332575ee9278641deaa53d833cd91a6b7cb
Nikitos <72337035+NikitosTop@users.noreply.github.com>
2020-10-12 14:42:07 +0300

    добавил доп решение

--- lab1/project6/project6.cpp
+++ lab1/project6/project6.cpp
@@ -14,4 +14,15 @@ int main()
 	printf("this century have: %lf second\n", ((century)+(day * 25)));
 	printf("this 1000 year have: %lf second\n", ((century * 10) + (day * 250)));
 	return 0;
+
+	int day, hour, minutes;
+	minutes = 60;
+	hour = minutes * 60;
+	day = hour * 24;
+	int this_year = day * 365;
+	printf("this year have: %d second\n", this_year + day);
+	int century = this_year * 100;
+	printf("this century have: %d second\n", ((century)+(day * 25)));
+	printf("this 1000 year have: %d second\n", ((century * 10) + (day * 250)));
+	return 0;
 }
