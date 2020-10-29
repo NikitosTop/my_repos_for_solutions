c3996f598984a5231f576d5292073aaabd8a5811
Nikitos <72337035+NikitosTop@users.noreply.github.com>
2020-10-29 13:06:36 +0300

    add code

--- lab2/project/project.cpp
+++ lab2/project/project.cpp
@@ -8,23 +8,23 @@ int main()
 	system("chcp 1251");
 	system("cls");
 
-	char name1[11], name2[11], name3[11];
+	/*char name1[11], name2[11], name3[11];
 
 	char type1, type2, type3;
 
 	int percent1, percent2, percent3;
 
-	float coef1, coef2, coef3;
+	float coef1, coef2, coef3;*/
 
-	/*char name1[11] = "Алюминий", name2[11] = "Стекловата", name3[11] = "Глина";
+	char name1[11] = "Алюминий", name2[11] = "Стекловата", name3[11] = "Глина";
 
 	char type1 = 'М', type2 = 'Т', type3 = 'Д';
 
 	int percent1 = 100, percent2 = 100, percent3 = 20;
 
-	float coef1 = 209.3, coef2 = 0.035, coef3 = 0.73;*/
+	float coef1 = 209.3, coef2 = 0.035, coef3 = 0.73;
 
-	printf("1. Введите: вещество, тип, влажность (%), коефициент >");
+	/*printf("1. Введите: вещество, тип, влажность (%), коефициент >");
 
 	scanf("%11s %c %d %f", &name1, &type1, &percent1, &coef1);
 
@@ -34,7 +34,7 @@ int main()
 
 	printf("3. Введите: вещество, тип, влажность (%), коефициент >");
 
-	scanf("%11s %c %d %f", &name3, &type3, &percent3, &coef3);
+	scanf("%11s %c %d %f", &name3, &type3, &percent3, &coef3);*/
 
 	printf("-----------------------------------------------\n");
 	printf("| Коэффициенты теплопроводимости материаллов |\n");
@@ -43,9 +43,9 @@ int main()
 	printf("|          |       |		   |         |\n");
 	printf("|----------|-------|-----------|-------------|\n");
 	/* вывод строк фактических данных */
-	printf("| %-11s| %-18c | 0-%-6d | %-3.1f |\n", name1, type1, percent1, coef1);
-	printf("| %-11s| %-18c | 0-%-6d | %-1.3f |\n", name2, type2, percent2, coef2);
-	printf("| %-11s| %-18c | 15-%-3d | %-1.2f |\n", name3, type3, percent3, coef3);
+	printf("| %-11s| %-2c | 0-%-6d | %-3.1f |\n", name1, type1, percent1, coef1);
+	printf("| %-11s| %-2c | 0-%-6d | %-1.3f |\n", name2, type2, percent2, coef2);
+	printf("| %-11s| %-2c | 15-%-5d | %-5.2f |\n", name3, type3, percent3, coef3);
 	/* вывод примечаний */
 	printf("|---------------------------------------------|\n");
 	printf("| Примечание: М - металлы, Т - термоизоляционные материалы |\n");
