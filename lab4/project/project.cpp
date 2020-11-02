7679c21672ac97bcd5d61b853b979f61e914439a
Nikitos <72337035+NikitosTop@users.noreply.github.com>
2020-11-02 20:06:56 +0300

    Add files via upload

---
+++ lab4/project/project.cpp
@@ -0,0 +1,19 @@
+﻿
+#include <iostream>
+
+int main()
+{
+    double x, y;
+    std::cout << "Enter x: " << std::endl;
+    std::cin >> x;
+    std::cout << "Enter y: " << std::endl;
+    std::cin >> y;
+
+    if ((x*x + y*y <= 1) && (x <= 0 && y <= 0) || (x <= 1 && x >= 0 && y >= 0 && y <= 1))
+    {
+        std::cout << "this is correct" << std::endl;
+    }
+
+    return main();
+}
+
