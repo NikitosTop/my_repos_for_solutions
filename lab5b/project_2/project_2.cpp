6c0c972b9351ea704e78b1a4163b48beba277cc6
Nikitos <72337035+NikitosTop@users.noreply.github.com>
2020-11-05 00:15:43 +0300

    Add files via upload

---
+++ lab5b/project_2/project_2.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <stdio.h>
+
+int main()
+{
+	unsigned int k;
+	printf("how many mushrooms do you have: ");
+	scanf_s("%d", &k);
+
+	if (k < 100)
+	{
+		printf("i have %d mushrooms", k);
+	}
+	else
+	{
+		printf("i call the police");
+	}
+}
