6bc8ff13411409ff05e1c905a617f55006c073b3
Nikitos <72337035+NikitosTop@users.noreply.github.com>
2020-11-09 12:47:33 +0300

    new solution

--- lab5b/project_2/project_2.cpp
+++ lab5b/project_2/project_2.cpp
@@ -17,7 +17,7 @@ int main()
 
 	}
 
-	if (number == 1 || number % 10 == 1)
+	if (number == 1 || number % 10 == 1 && number != 11)
 	{
 		printf("у меня %d гриб\n", number);
 	}
@@ -27,7 +27,7 @@ int main()
 		printf("у меня %d гриба\n", number);
 	}
 
-	if (number > 4 && number < 10 || number % 10 == 0 || number % 10 == 5 || number % 10 == 6 || number % 10 == 7 || number % 10 == 8 || number % 10 == 9)
+	if (number > 4 && number < 10 || number == 11 || number % 10 == 0 || number % 10 == 5 || number % 10 == 6 || number % 10 == 7 || number % 10 == 8 || number % 10 == 9)
 	{
 		printf("у меня %d грибов\n", number);
 	}
