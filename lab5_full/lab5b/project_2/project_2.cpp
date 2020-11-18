eeb1ea5f1cdb726e2f4c25b4be9dd684a6d9a45d
Nikitos <72337035+NikitosTop@users.noreply.github.com>
2020-11-18 23:29:24 +0300

    сделал

--- lab5_full/lab5b/project_2/project_2.cpp
+++ lab5_full/lab5b/project_2/project_2.cpp
@@ -17,17 +17,17 @@ int main()
 
 	}
 
-	if (number == 1 || number % 10 == 1 && number != 11)
+	if (number == 1 || number % 10 == 1 != (number % 100 == 11))
 	{
 		printf("у меня %d гриб\n", number);
 	}
 	
-	if (number > 1 && number <= 4 && number != 12 && number != 13 && number != 14 || number % 10 == 2 || number % 10 == 3 || number % 10 == 4)
+	if (number > 1 && number <= 4 && (number != 12 || number != 13 || number != 14) && (number % 10 == 2 || number % 10 == 3 || number % 10 == 4))
 	{
 		printf("у меня %d гриба\n", number);
 	}
 
-	if (number > 4 && number < 10 || number == 11 || number == 12 || number == 13 || number == 14 || number % 10 == 0 || number % 10 == 5 || number % 10 == 6 || number % 10 == 7 || number % 10 == 8 || number % 10 == 9)
+	if (number > 4 && number < 10 || number % 100 == 11 || number == 12 || number == 13 || number == 14 || number % 10 == 0 || number % 10 == 5 || number % 10 == 6 || number % 10 == 7 || number % 10 == 8 || number % 10 == 9)
 	{
 		printf("у меня %d грибов\n", number);
 	}
