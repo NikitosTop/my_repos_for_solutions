af172a109547891ce54de362a0b06b72cae4523d
Nikitos <72337035+NikitosTop@users.noreply.github.com>
2020-11-24 23:12:05 +0300

    наконец-то доделал

--- lab5_full/lab5b/project_2/project_2.cpp
+++ lab5_full/lab5b/project_2/project_2.cpp
@@ -11,25 +11,22 @@ int main()
 	printf("Сколько у вас грибов: ");
 	scanf_s("%d", &number);
 
-	if (number == 0)
+	if (number % 10 == 1 && number % 100 != 11)
 	{
-		printf("у меня %d грибов\n", number);
-
-	}
-
-	if (number == 1 || number % 10 == 1 != (number % 100 == 11))
-	{
-		printf("у меня %d гриб\n", number);
+		printf("у меня %d гриб\n", number); // 1 21 31 ...
 	}
 	
-	if (number > 1 && number <= 4 && (number != 12 || number != 13 || number != 14) || (number % 10 == 2 || number % 10 == 3 || number % 10 == 4))
+	if (number % 10 == 2 || number % 10 == 3 || number % 10 == 4)
 	{
-		printf("у меня %d гриба\n", number);
+		if (number % 100 != 12 && number % 100 != 13 && number % 100 != 14)
+		{
+			printf("у меня %d гриба\n", number); // 2 3 4 22 33 44
+		}
 	}
 
-	if (number > 4 && number < 10 || number % 100 == 11 || number == 12 || number == 13 || number == 14 || number % 10 == 0 || number % 10 == 5 || number % 10 == 6 || number % 10 == 7 || number % 10 == 8 || number % 10 == 9 || number % 10 == 12)
+	if (number % 100 == 11 || number % 100 == 12 || number % 100 == 13 || number % 100 == 14 || number % 10 > 4 && number % 10 < 10 || number % 10 == 0)
 	{
-		printf("у меня %d грибов\n", number);
+		printf("у меня %d грибов\n", number); // 11 111 12 13 14 5 6 7 8 9 10
 	}
 
 	return main();
