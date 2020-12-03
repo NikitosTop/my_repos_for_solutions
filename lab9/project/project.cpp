23b890ed93de50c00f9373173a249300a88e3205
Nikitos <72337035+NikitosTop@users.noreply.github.com>
2020-12-03 12:18:59 +0300

    добавил две лабы

---
+++ lab9/project/project.cpp
@@ -0,0 +1,53 @@
+﻿#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+int main()
+{
+	const int n = 9;
+
+	int Arr[n][n] = { {},{} };
+	int j = 0, k = 0, i = 1, col = 4, row = 4;
+	for (int amount = 1; amount < 6; amount++)
+	{
+		for (j = 1; j < 8 + k - 6; j++, ++row)
+		{
+			Arr[row][col] = i;
+			i++;
+		}
+		if (i > 81)
+		{
+			break;
+		}
+		else {
+			for (j = 1; j < 8 + k - 6; j++, --col)
+			{
+				Arr[row][col] = i;
+				i++;
+			}
+			k++;
+			for (j = 1; j < 8 + k - 6; j++, --row)
+			{
+				Arr[row][col] = i;
+				i++;
+			}
+			for (j = 1; j < 8 + k - 6; j++, ++col)
+			{
+				Arr[row][col] = i;
+				i++;
+			}
+			k++;
+		}
+	}
+
+	for (int row = 0; row < n; row++)
+	{
+		for (int col = 0; col < n; col++)
+		{
+			cout << Arr[row][col] << "\t";
+		}
+		cout << endl;
+	}
+
+}
---
+++ lab9a/project/project.cpp
@@ -0,0 +1,112 @@
+﻿#include <iostream>
+#include <stdio.h>
+#include <time.h>
+
+using namespace std;
+
+int main()
+{
+	srand(time(NULL));
+
+	const int n = 5;
+
+	int Arr[n][n] = { {},{} };
+
+	for (int row = 0; row < 5; row++)
+	{
+		for (int col = 0; col < 5; col++)
+		{
+			Arr[row][col] = 1 + rand() % 9;
+		}
+	}
+
+	for (int row = 0; row < 5; row++)
+	{
+		for (int col = 0; col < 5; col++)
+		{
+			cout << Arr[row][col] << " ";
+		}
+		cout << endl;
+	}
+
+	cout << "Enter row(1) or col(0): ";
+	int change = 0;
+	cin >> change;
+
+	if (change == 1)
+	{
+		int i = 0;
+
+		for (int row = 0; row < 5; row++)
+		{
+			int max = Arr[row][0];
+			for (int col = 1; col < 5; col++)
+			{
+				if (Arr[row][col] > max)
+				{
+					max = Arr[row][col];
+				}
+			}
+
+			for (int col = 0; col < 5; col++)
+			{
+				if (Arr[row][col] == max)
+				{
+					i = Arr[row][0];
+					Arr[row][0] = Arr[row][col];
+					Arr[row][col] = i;
+				}
+			}
+
+		}
+
+		cout << endl;
+
+		for (int row = 0; row < 5; row++)
+		{
+			for (int col = 0; col < 5; col++)
+			{
+				cout << Arr[row][col] << " ";
+			}
+			cout << endl;
+		}
+	}
+	else
+	{
+		int i = 0;
+
+		for (int col = 0; col < 5; col++)
+		{
+			int max = Arr[0][col];
+			for (int row = 1; row < 5; row++)
+			{
+				if (Arr[row][col] > max)
+				{
+					max = Arr[row][col];
+				}
+			}
+
+			for (int row = 0; row < 5; row++)
+			{
+				if (Arr[row][col] == max)
+				{
+					i = Arr[0][col];
+					Arr[0][col] = Arr[row][col];
+					Arr[row][col] = i;
+				}
+			}
+
+		}
+
+		cout << endl;
+
+		for (int row = 0; row < 5; row++)
+		{
+			for (int col = 0; col < 5; col++)
+			{
+				cout << Arr[row][col] << " ";
+			}
+			cout << endl;
+		}
+	}
+}
