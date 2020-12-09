b3497ef3f6b41ea059f7bcd2c7b435e38b65afbb
Nikitos <72337035+NikitosTop@users.noreply.github.com>
2020-12-10 01:07:36 +0300

    правильное решение

--- lab10b/project/project.cpp
+++ lab10b/project/project.cpp
@@ -2,46 +2,88 @@
 #include <stdio.h>
 #include <time.h>
 
-//int double_arr(int row, int col)
-//{
-//	int Arr[100][100] = { {},{} };
-//
-//	int size = row * col;
-//
-//	for (int i = 0;i < row; i++)
-//	{
-//		for (int j = 0; j < col; j++)
-//		{
-//			int number = 0;
-//			std::cout << "Enter number: ";
-//			std::cin >> number;
-//			Arr[i][j] = number;
-//		}
-//	}
-//}
-//
-//int write_array(int row, int col)
-//{
-//	std::cout << std::endl;
-//
-//	int Arr[100][100] = {};
-//
-//	for (int i = 0; i < row; i++)
-//	{
-//		for (int j = 0; j < col; j++)
-//		{
-//			std::cout << Arr[i][j] << "\t";
-//		}
-//		std::cout << std::endl;
-//	}4
-//	
-//	return 0;
-//}
-////
-////int positivs_elements(int Arr)
-////{
-////
-////}
+int write_double_array(int row, int col, int Arr[])
+{
+
+	int Arr_2[100][100] = { {} };
+
+	int term = 0;
+	for (int i = 0; i < row; i++)
+	{
+		for (int j = 0; j < col; j++)
+		{
+			Arr_2[i][j] = Arr[term];
+			term++;
+		}
+	}
+
+	std::cout << std::endl;
+
+	for (int i = 0; i < row; i++)
+	{
+		for (int j = 0; j < col; j++)
+		{
+			std::cout << Arr_2[i][j] << "\t";
+		}
+		std::cout << std::endl;
+	}
+
+	return 0;
+}
+
+int positive_elements(int row, int col, int Arr[])
+{
+	int Arr_2[100][100] = { {} };
+
+	int term = 0;
+	for (int i = 0; i < row; i++)
+	{
+		for (int j = 0; j < col; j++)
+		{
+			Arr_2[i][j] = Arr[term];
+			term++;
+		}
+	}
+
+	int numbers = 0;
+
+	for (int i = 0; i < row; i++)
+	{
+		for (int j = 0; j < col; j++)
+		{
+			if (Arr_2[i][j] > 0)
+			{
+				numbers++;
+			}
+		}
+		std::cout << std::endl;
+		std::cout << "Row" << "(" << i + 1 << ")" << numbers;
+		std::cout << std::endl;
+		numbers = 0;
+	}
+
+	return 0;
+}
+
+int double_array(int row, int col)
+{
+	srand(time(NULL));
+
+	int Arr[100] = {};
+
+	int size = row * col;
+
+	for (int i = 0; i < size; i++)
+	{
+		Arr[i] = -10 + rand() % 20;
+	}
+
+	write_double_array(row, col, Arr);
+
+	positive_elements(row, col, Arr);
+
+	return 0;
+}
 
 int main()
 {
@@ -54,43 +96,5 @@ int main()
 	std::cout << "Enter col: ";
 	std::cin >> col;
 
-	int Arr[100][100] = { {},{} };
-
-	int size = row * col;
-
-	for (int i = 0; i < row; i++)
-	{
-		for (int j = 0; j < col; j++)
-		{
-			Arr[i][j] = -10 + rand() % 20;
-		}
-	}
-
-	std::cout << std::endl;
-
-	for (int i = 0; i < row; i++)
-	{
-		for (int j = 0; j < col; j++)
-		{
-			std::cout << Arr[i][j] << "\t";
-		}
-		std::cout << std::endl;
-	}
-
-	int numbers = 0;
-
-	for (int i = 0; i < row; i++)
-	{
-		for (int j = 0; j < col; j++)
-		{
-			if (Arr[i][j] > 0)
-			{
-				numbers++;
-			}
-		}
-		std::cout << std::endl;
-		std::cout << "Row" << "(" << i + 1 << ")" << numbers;
-		std::cout << std::endl;
-		numbers = 0;
-	}
+	double_array(row, col);
 }
