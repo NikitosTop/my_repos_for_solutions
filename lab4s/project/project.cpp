f1c8ed4597e185ca8715930e03d15b4fcf7184ea
Nikitos <72337035+NikitosTop@users.noreply.github.com>
2020-12-14 12:23:30 +0300

    ++

---
+++ lab4s/project/project.cpp
@@ -0,0 +1,55 @@
+﻿#include <stdio.h>
+#include <iostream>
+
+int main(void) {
+
+    setlocale(LC_ALL, "Russian");
+    int number = 0;
+    std::cout << "запаковать (0) или распаковать(1): ";
+    std::cin >> number;
+    if (number == 0)
+    {
+        char c; 
+        char f; 
+        int b; 
+        unsigned char n; 
+        unsigned int UnitStateWord; 
+         /* ввод составных частей */
+        printf("Введите номер канала таймера (0 - 3) >");
+        scanf_s("%d", &c);
+        printf("Введите форму сигнала (0 - 3) >");
+        scanf_s("%d", &f);
+        printf("Введите коэффициент деления опорной частоты: 0-4095 >");
+        scanf_s("%d", &b);
+        /* формирование упакованного кода */
+        UnitStateWord = ((unsigned int)c & 0x03) << 14;
+        UnitStateWord |= ((unsigned int)f & 0x03) << 12;
+        UnitStateWord |= ((unsigned long int)b & 0xFFF) << 0;
+        /* вывод результата */
+        printf("\nУправляющее слово программируемого таймера = %04x\n",
+            UnitStateWord);
+    }
+    else
+    {
+        char c; 
+        char f; 
+        int b; 
+        unsigned char n; 
+        unsigned long int UnitStateWord;
+         /* yправляющее слово программируемого таймера */
+        printf("Введите yправляющее слово программируемого таймера \n");
+        printf("(16-ричное число от 0 до 0xFFFF) >");
+        scanf_s("%x", &UnitStateWord);
+        /* Выделение составных частей */
+        c = (UnitStateWord >> 14) & 0x03;
+        f = (UnitStateWord >> 12) & 0x03;
+        b = (UnitStateWord >> 0) & 0xFFF;
+        /* вывод результатов */
+        putchar('\n');
+        printf("номер канала таймера = %d\n", c);
+        printf("форма сигнала = %d\n", f);
+        printf("коэффициент деления опорной частоты = %d\n", b);
+
+    }
+    return 0;
+}
---
+++ lab8a/project/project.cpp
@@ -0,0 +1,96 @@
+﻿#include <iostream>
+#include <stdio.h>
+#include <time.h>
+using namespace std;
+
+int main() 
+{
+	srand(time(NULL));
+	
+	const int n = 100;
+
+	int Array[n] = {};
+
+	int size = 10;
+
+	int seat_number = 0;
+
+	while(true)
+	{
+		int question = 0;
+		cout << "Do you smoke?(yes 1)(no 0): " << endl;
+		cin >> question;
+		if (question == 1)
+		{
+			int seat_number = 1 + rand() % 5;
+			if (Array[seat_number - 1] == 1)
+			{
+				cout << "this seat has busy" << endl;
+				continue;
+			}
+			else
+			{
+				Array[seat_number - 1] = 1;
+			}
+			cout << "You seat is " << seat_number << endl;
+			int term = 0;
+			for (int j = 0; j < 5; j++)
+			{
+				if (Array[j] == 1)
+				{
+					term++;
+				}
+			}
+			if (term == 5)
+			{
+				cout << "All place have busy" << endl;
+				continue;
+			}
+			
+		}
+		else
+		{
+			int seat_number = 6 + rand() % 5;
+			if (Array[seat_number - 1] == 1)
+			{
+				cout << "this seat has busy" << endl;
+				continue;
+			}
+			else 
+			{
+				Array[seat_number - 1] = 1;
+			}
+			cout << "You seat is " << seat_number << endl;
+			int term = 0;
+			
+
+			for (int j = 54; j < 10; j++)
+			{
+				if (Array[j] == 1)
+				{
+					term++;
+				}
+			}
+			if (term == 5)
+			{
+				cout << "All place have busy" << endl;
+				continue;
+			}
+		}
+		seat_number++;
+		int term = 0;
+		for (int j = 0; j < 10; j++)
+		{
+			if (Array[j] == 1)
+			{
+				term++;
+			}
+		}
+		if (term == 10)
+		{
+			cout << "All place have busy" << endl;
+			break;
+		}
+	}
+
+}
