8284f8a575db390451faa3196b757928468f1e0f
Nikitos <72337035+NikitosTop@users.noreply.github.com>
2020-12-18 14:03:10 +0200

    edit

--- calculation-graphic-task/project/project.cpp
+++ calculation-graphic-task/project/project.cpp
@@ -102,80 +102,104 @@ int main()
 	std::cout << "Выберите тип сортировки (1 - PancakeSort) или (2 - Odd_even_sort): ";
 	std::cin >> type_arr;
 
-	double during_arr[9] = {};
+	double during_arr[100] = {};
 
 	int during_term = 0;
 
-	for (int a = 0; a < 9; a++)
+	srand(time(NULL));
+	const int n = 45;
+	int Arr[n] = {};
+
+	int amount_numbers = 0;
+	std::cout << std::endl;
+	std::cout << "Введите количество элементов массива: ";
+	std::cin >> amount_numbers;
+
+
+	if (arr == 1)
 	{
-		srand(time(NULL));
-		const int n = 45;
-		int Arr[n] = {};
+		for (int i = 0; i < amount_numbers; i++)
+		{
+			Arr[i] = i + 1;
+		}
+		for (int i = 0; i < amount_numbers; i++)
+		{
+			std::cout << Arr[i] << " ";
+		}
+	}
+	else if (arr == 2)
+	{
+		for (int i = 0; i < amount_numbers; i++)
+		{
+			Arr[i] = 1 + rand() % 99;
+		}
 
-		int amount_numbers = 0;
+		for (int i = 0; i < amount_numbers; i++)
+		{
+			std::cout << Arr[i] << " ";
+		}
+	}
+	else
+	{
+		int term_number = amount_numbers;
+
+		for (int i = 0; i < amount_numbers; i++)
+		{
+			Arr[i] = term_number--;
+		}
+
+		for (int i = 0; i < amount_numbers; i++)
+		{
+			std::cout << Arr[i] << " ";
+		}
+	}
+
+	int* new_arr = (int*)malloc(sizeof(int) * n);
+
+	for (int i = 0; i < n; i++)
+	{
+		new_arr[i] = Arr[i];
+	}
+
+	if (type_arr == 1)
+	{
+		int* target;
+
 		std::cout << std::endl;
-		std::cout << "Введите количество элементов массива: ";
-		std::cin >> amount_numbers;
 
+		target = PancakeSort(amount_numbers, new_arr);
 
-		if (arr == 1)
+		for (int i = 0; i < amount_numbers; i++)
 		{
-			for (int i = 0; i < amount_numbers; i++)
-			{
-				Arr[i] = i + 1;
-			}
-
-			for (int i = 0; i < amount_numbers; i++)
-			{
-				std::cout << Arr[i] << " ";
-			}
-		}
-		else if (arr == 2)
-		{
-			for (int i = 0; i < amount_numbers; i++)
-			{
-				Arr[i] = 1 + rand() % 99;
-			}
-
-			for (int i = 0; i < amount_numbers; i++)
-			{
-				std::cout << Arr[i] << " ";
-			}
-		}
-		else
-		{
-			int term_number = amount_numbers;
-
-			for (int i = 0; i < amount_numbers; i++)
-			{
-				Arr[i] = term_number--;
-			}
-
-			for (int i = 0; i < amount_numbers; i++)
-			{
-				std::cout << Arr[i] << " ";
-			}
+			std::cout << target[i] << " ";
 		}
 
-		int* new_arr = (int*)malloc(sizeof(int) * n);
+	}
 
-		for (int i = 0; i < n; i++)
+	else
+	{
+		int* target;
+
+		std::cout << std::endl;
+
+		target = Odd_even_sort(amount_numbers, new_arr);
+
+		for (int i = 0; i < amount_numbers; i++)
 		{
-			new_arr[i] = Arr[i];
+			std::cout << target[i] << " ";
 		}
+	}
+
+	int tick = 0;
+
+	while (tick < 100)
+	{
 
 		if (type_arr == 1)
 		{
 			int* target;
 
-			std::cout << std::endl;
-
 			target = PancakeSort(amount_numbers, new_arr);
-
-			for (int i = 0; i < amount_numbers; i++)
-			{
-				std::cout << target[i] << " ";
-			}
 
 			auto start = std::chrono::high_resolution_clock::now();
 
@@ -189,18 +213,12 @@ int main()
 			during_term++;
 
 		}
+
 		else
 		{
 			int* target;
 
-			std::cout << std::endl;
-
 			target = Odd_even_sort(amount_numbers, new_arr);
-
-			for (int i = 0; i < amount_numbers; i++)
-			{
-				std::cout << target[i] << " ";
-			}
 
 			auto start = std::chrono::high_resolution_clock::now();
 
@@ -215,15 +233,49 @@ int main()
 
 		}
 
+		tick++;
+
 	}
 
 	std::cout << std::endl;
 
-	for (int i = 0; i < 9; i++)
-	{
-		std::cout << " | " << during_arr[i] << " | ";
+	double sum = 0;
 
+	double min = during_arr[0];
+	double max = during_arr[0];
+
+	for (int i = 0; i < 100; i++)
+	{
+		if (during_arr[i] > max)
+		{
+			max = during_arr[i];
+		}
 	}
+
+	double temp = max;
+	max = during_arr[99];
+	during_arr[99] = temp;
+
+	for (int i = 0; i < 100; i++)
+	{
+		if (during_arr[i] < min)
+		{
+			min = during_arr[i];
+		}
+	}
+
+	double temp_2 = min;
+	min = during_arr[0];
+	during_arr[0] = temp;
+
+	for (int i = 1; i < 99; i++)
+	{
+		sum += during_arr[i];
+	}
+
+	std::cout << std::endl;
+
+	printf("Average time: %2.9lf", sum / 98);
 
 }
 
